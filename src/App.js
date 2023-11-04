19a5ee38f05ec48df4e3d9f109aa67d520a12d66
muhammadafsakkh <afsalhp2002@gmail.com>
2023-11-04 09:59:00 -0700

    Quiz-App

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
+
+import { useState } from 'react';
 import './App.css';
+import questions from './assets/questions';
+
 
 function App() {
+  const [questionIdx,setQuestionIdx] = useState(0)
+  const [score,setScore] = useState(0)
+  const [showScore,setShowScore] = useState(false)
+  const currQuestion = questions[questionIdx]
+  
+  const selectOption = (idx) => {
+    console.log(idx);
+    if (currQuestion.answer === idx) {
+      setScore(score + 1)
+    }
+    const nextQ = questionIdx +1
+    if (nextQ < questions.length) {
+      setQuestionIdx(questionIdx + 1)
+    }else{
+      setShowScore(true)
+    }
+  }
+
+
+  const reset =()=>{
+    setQuestionIdx(0)
+    setScore(0)
+    setShowScore(false)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="quiz-container">
+      {
+        showScore ?(
+          <>
+         <h1>Your Score is: {score}</h1>
+         <button onClick={reset}>Restart Quiz</button>
+         </>
+         ):(
+          <div className='quiz-container-question'>
+          <p>{currQuestion.question}</p>
+          <div className='quiz-container-options'>
+            <ul className='quiz-container-ul'>
+              {currQuestion.options.map((option,i)=>{
+                return  <li className='quiz-container-li' onClick={()=>selectOption(i)}>{option}</li>
+              })}
+             
+            </ul>
+          </div>
+         </div>
+        )
+      }
+    
     </div>
   );
 }
---
+++ src/assets/questions.js
@@ -0,0 +1,52 @@
+export default [
+    {
+        question: "1st Question : Who was the first to discover india?",
+        options: ["1. Sardar Vallabhai Patel","2. Vasco da Gama","3. C.V Raman","4. Subash Chandra Bose"],
+        answer: 1,
+    },
+    {
+        question: "2nd Question : Who is known as the “Iron Man” of India?",
+        options: ["1. Mahatma Gandhi","2. A. P. J. Abdul Kalam","3. Sardar Vallabhbhai Patel","4. Bhimrao Ramji Ambedkar"],
+        answer: 2,
+    },
+    {
+        question: "3rd Question : What is the Capital of India?",
+        options: ["1. Varanasi","2. Agra","3. Mumbai","4. New Delhi"],
+        answer: 3,
+    },
+    {
+        question: "4th Question : Which state is also known as the “fruit bowl” of India?",
+        options: ["1. Himachal Pradesh","2. Delhi","3. Kashmir","4. kerala"],
+        answer: 0,
+    },
+    {
+        question: "5th Question : Which is the national sport of India?",
+        options: ["1. Badminton","2. Football","3. Cricket","4. Hockey"],
+        answer: 3,
+    },
+    {
+        question: "6th Question : Which state is also known as the “land of rising sun”?",
+        options: ["1. Andra","2. Arunachal Pradesh","3. Karnataka","4. Tamilnadu"],
+        answer: 1,
+    },
+    {
+        question: "7th Question : Where is the Taj Mahal located?",
+        options: ["1. Agra","2. New Delhi","3. Singapore","4. Angamaly"],
+        answer: 0,
+    },
+    {
+        question: "8th Question : Which Indian state has the smallest coastline?",
+        options: ["1. Rajasthan","2. Gujarat","3. Goa","4. Bihar"],
+        answer: 2,
+    },
+    {
+        question: "9th Question : What is the national heritage animal of India?",
+        options: ["1. Tiger","2. Lion","3. Jirafa","4. Elephant"],
+        answer: 3,
+    },
+    {
+        question: "10th Question : In which state is the International Kite Festival celebrated?",
+        options: ["1. Haryana","2. Gujarat","3. Uttar Pradesh","4. West Bengal"],
+        answer: 1,
+    },
+]
